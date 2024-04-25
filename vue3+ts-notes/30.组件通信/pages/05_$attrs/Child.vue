15bb341520855b688f8039b3d858eb3bcf64cc22
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2024-04-25 09:28:50 +0800

    Add files via upload

---
+++ vue3+ts-notes/30.组件通信/pages/05_$attrs/Child.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="child">
+		<h3>子组件</h3>
+		<GrandChild v-bind="$attrs"/>
+	</div>
+</template>
+
+<script setup lang="ts" name="Child">
+	import GrandChild from './GrandChild.vue'
+	// $attrs 包含了父组件传递给子组件的除了props之外的所有属性
+	// 子组件可以直接使用$attrs来获取父组件传递的属性
+	// 子组件也可以通过v-bind="$attrs"将$attrs传递给孙子组件
+	// 孙子组件也可以通过v-bind="$attrs"将$attrs传递给曾孙组件
+	// 以此类推，直到最底层的组件
+</script>
+
+<style scoped>
+	.child{
+		margin-top: 20px;
+		background-color: skyblue;
+		padding: 20px;
+		border-radius: 10px;
+		box-shadow: 0 0 10px black;
+	}
+</style>
